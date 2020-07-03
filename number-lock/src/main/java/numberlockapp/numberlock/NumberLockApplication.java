606aae408d43025961f765fc8f47d80b234a0f2e
Sofia <sofia.zholubak@gmail.com>
2020-07-03 16:46:35 +0200

    initial commit

---
+++ number-lock/src/main/java/numberlockapp/numberlock/NumberLockApplication.java
@@ -0,0 +1,13 @@
+package numberlockapp.numberlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberLockApplication.class, args);
+	}
+
+}
